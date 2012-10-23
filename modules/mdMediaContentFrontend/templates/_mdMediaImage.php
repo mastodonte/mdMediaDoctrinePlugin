9e396dc2e38beb4458ae966ac226c318ad8d1fe6
maui <niko@mauitz.com>
2012-10-23 16:50:21 -0200

    support html param for image tag information

--- modules/mdMediaContentFrontend/templates/_mdMediaImage.php
+++ modules/mdMediaContentFrontend/templates/_mdMediaImage.php
@@ -1,2 +1,8 @@
-<img <?php echo (isset($id) ? 'id="' . $id . '"' : ''); ?> src="<?php echo $mediaConcrete->getUrl(array(mdWebOptions::WIDTH => $width, mdWebOptions::HEIGHT => $height, mdWebOptions::EXACT_DIMENTIONS => $EXACT_DIMENTIONS, mdWebOptions::CODE => $CODE)); ?>" />
+<img <?php 
+	echo (isset($id) ? 'id="' . $id . '"' : ''); 
+?> src="<?php 
+	echo $mediaConcrete->getUrl(array(mdWebOptions::WIDTH => $width, mdWebOptions::HEIGHT => $height, mdWebOptions::EXACT_DIMENTIONS => $EXACT_DIMENTIONS, mdWebOptions::CODE => $CODE)); 
+?>" <?php 
+	echo (isset($html) ? $html : ''); 
+?>/>
 
